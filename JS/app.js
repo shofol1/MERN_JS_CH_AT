afd970e53daaf0fc3ea6e11e4464e63b0f6967bc
shofol <18103156@iubat.edu>
2022-09-26 16:41:55 +0600

    map set weakmap weakset

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,72 @@
+//regular ecpression
+
 //test,source,match,search,exec,replace
 
 // const regExp = /shafol/i;
-const regExp = /shafol/gi;
+// const regExp = /shafol/gi;
 
 // console.log(regExp.test("Hi i am Ashikul Shafol"));
 
 // console.log(regExp.source);
 // console.log(regExp.exec("hi i am shafol"));
-const str = "hi i am shafol shafol shafol";
+// const str = "hi i am shafol shafol shafol";
 // console.log(str.match(regExp));
 // console.log(str.search(regExp));
-console.log(str.replace(regExp, "Ashikul"));
+// console.log(str.replace(regExp, "Ashikul"));
+
+// array data structure
+
+//1. set
+
+// const set = new Set();
+// set.add(100);
+// set.add(200);
+// set.add(300);
+// set.add(300); //duplicate value discurded
+// console.log(set);
+// set.delete(300);
+// console.log(set);
+// set.clear();
+// console.log(set);
+
+//1. map
+
+// const map = new Map();
+// map.set("1", "Mango");
+// map.set("2", "Banana");
+// map.set("3", "orange");
+// map.set("4", "lichu");
+
+// console.log(map);
+
+// map.forEach((el, i) => console.log(i, el));
+
+// console.log(map.has("2"));
+
+// map.delete("1");
+// console.log(map);
+
+// map.clear();
+// console.log(map);
+
+//3. weak map and weak set
+
+const ws = new WeakSet();
+const object1 = {};
+const object2 = {};
+
+ws.add(object1);
+ws.add(object2);
+console.log(ws);
+
+console.log(ws.has(object1));
+ws.delete(object1);
+console.log(ws.has(object1));
+
+const wm = new WeakMap();
+
+wm.set(object1, 12121);
+console.log(wm.has(object1));
+console.log(wm.get(object1));
+wm.delete(object1);
+console.log(wm.has(object1));
